46af8a6c6229ac9dd5df285b31148942fd5aa054
Unclephil <koenigphil@gmail.com>
2023-06-15 13:53:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+node-red-contrib-fr24tube
+=========================
+
+> Based on zuhito node-red-contrib-flightradar24
+
+Node-RED node to retrieve plane locations from flightradar24 service, but only in a square tube based on coordinates and radius
+
+## Ph Koenig addendum:
+- set a square distance around lat/long to get a specific number of plane.
+        - this add geolib dependencies
+- return only plane in this tube.
+- add bot informations to msg.
+- protect the "non payload" of original msg.
+
+## Install
+-------
+Run the following command in your Node-RED user directory - typically `~/.node-red`
+
+        npm install node-red-contrib-fr24tube
+
+
---
+++ node.js
@@ -0,0 +1,53 @@
+var radar = require('flightradar24-client/lib/radar');
+var geolib = require('geolib')
+
+module.exports = function (RED) {
+    function Flightradar24NodePKO(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function (msg) {
+            var lat, lon, rad;
+            if (config.latType === 'num') {
+                lat = Number(config.lat);
+            } else {
+                lat = Number(RED.util.getMessageProperty(msg, config.lat));
+            }
+            if (config.lonType === 'num') {
+                lon = Number(config.lon);
+            } else {
+                lon = Number(RED.util.getMessageProperty(msg, config.lon));
+            }
+            if (config.radType === 'num') {
+                rad = Number(config.rad);
+            } else {
+                rad = Number(RED.util.getMessageProperty(msg, config.rad));
+            }
+
+            // calculate bounds
+            var bounds=geolib.getBoundsOfDistance( { latitude: lat, longitude: lon },rad );
+
+            // get flight in the tube    
+            radar(bounds[1].latitude, bounds[0].longitude, bounds[0].latitude, bounds[1].longitude).then(function (data) {
+                data.forEach(function (flight) {
+                    var m = RED.util.cloneMessage(msg);
+                    m.payload = flight;
+                    m.payload.lat = flight.latitude;
+                    delete m.payload.latitude;
+                    m.payload.lon = flight.longitude;
+                    delete m.payload.longitude;
+                    m.payload.name = flight.callsign || flight.registration || flight.modeSCode;
+                    m.payload.icon = "plane";
+                    m.payload.iconColor = "#F9DF39";
+                    m.payload.layer = "FlightRadar24";
+                    m.payload.botlat = lat ;
+                    m.payload.botlon = lon;
+                    m.payload.botrad = rad;
+                    node.send(m);
+                });
+            }).catch(function (error) {
+                node.error(error, msg);
+            });
+        });
+    }
+    RED.nodes.registerType("flightradar24", Flightradar24NodePKO);
+};
